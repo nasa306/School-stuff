570bdf54d6a5459d3e56a29e9944de010045a424
nasa306 <164401796+nasa306@users.noreply.github.com>
2025-01-30 20:09:48 -0800

    Update subscription.js

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -26,7 +26,6 @@ document.getElementById("subscription-form").addEventListener("submit", function
       if (data.trim() === "Success") {
         sentMessage.style.display = "block";
         document.getElementById("subscription-form").reset(); // Reset form
-        window.location.href = "https://nasa306.github.io/School-stuff/thank-you.html"
       } else {
         errorMessage.style.display = "block";
       }
@@ -35,6 +34,5 @@ document.getElementById("subscription-form").addEventListener("submit", function
       loading.style.display = "none";
       errorMessage.style.display = "block";
       console.error("Error:", error);
-      window.location.href = "https://nasa306.github.io/School-stuff/thank-you.html"
     });
 });
